4265efd4b59fb075c4fcb390f09e1c4a97e78717
Mauricio Aniche <mauricioaniche@gmail.com>
2020-06-07 23:47:59 +0200

    make code to compile again

--- src/main/java/nl/tudelft/serg/slrcrawler/library/LibraryCrawler.java
+++ src/main/java/nl/tudelft/serg/slrcrawler/library/LibraryCrawler.java
@@ -3,5 +3,5 @@ package nl.tudelft.serg.slrcrawler.library;
 import nl.tudelft.serg.slrcrawler.HtmlPage;
 
 public interface LibraryCrawler {
-    HtmlPage downloadPage(String keywords, int pageNumber) throws PageNotFoundException;
+    HtmlPage downloadPage(String keywords, int pageNumber);
 }
